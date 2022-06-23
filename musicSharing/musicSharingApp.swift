3aa8ccb49376c54c6a45b4dc8bf94d2c83a79064
Gianmembreno <66712565+Gianmembreno@users.noreply.github.com>
2022-06-23 18:30:30 -0400

    Initial Commit

---
+++ musicSharing/musicSharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicSharingApp.swift
+//  musicSharing
+//
+//  Created by Gian Membreno on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct musicSharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
